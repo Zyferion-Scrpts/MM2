eac50b5c8add8a01b01d18b7bad6d36a26ef0747
Zyferion-Scrpts <uzilia769@gmail.com>
2025-07-26 12:43:33 +0800

    Update Script.lua

--- Script.lua
+++ Script.lua
@@ -1,1 +1,2 @@
 loadstring(game:HttpGet("https://api.rubis.app/v2/scrap/UJ27GadqlYlk9kR7/raw", true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Joystickplays/psychic-octo-invention/main/source/yarhm/1.19/yarhm.lua"))()
